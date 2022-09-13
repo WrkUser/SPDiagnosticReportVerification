d3593295c1e5807f876e161c66c008927b63cc5c
WrkUser <102183510+WrkUser@users.noreply.github.com>
2022-09-12 19:21:30 -0500

    Update Verify-DiagnosticReport.sh

--- Verify-DiagnosticReport.sh
+++ Verify-DiagnosticReport.sh
@@ -1,134 +1,201 @@
 #!/usr/local/bin/bash
 
-#************************************************
+#***********************************
 # FUNCTIONS
-#************************************************
-function get_options() {
+#***********************************
+function __main__() {
+  # VARIABLES
+  local option="";
+  local file_name="";
+  local auth_pivnet_url="https://network.tanzu.vmware.com/api/v2/authentication";
+  local product_pivnet_url="https://network.tanzu.vmware.com/api/v2/product_details";
+  local product_name="";
+  local product_version="";
+  local product_pivnet_slug="";
+  local obj_jq="";
+  local counter=0;
+  
+  # BODY
+  rm -f diagnostic_report_all*
+  rm -f diagnostic_report_eogs*
+  rm -f diagnostic_report_supported*
+  
   while getopts ":hk:f:" option
   do
     case ${option} in
       f)
-        file_name_json="${OPTARG}";
-        verify_json_file
-        ;;
+        if verify_json_file "${OPTARG}"
+        then
+          file_name="${OPTARG}"
+        else
+          echo "Error with the return value while verifying json file."
+        fi
+      ;;
       k)
-        api_key="${OPTARG}";
-        verify_api_key
-        ;;
+        clear;
+        echo "This API feature feature is pending.";
+        echo "Please remove this option, and try again.";
+        exit;
+      ;;
       h)
         clear;
         echo "Usage:";
-        echo "           Mandatory                        [-f] Path to 'diagnostic_report.json'";
-        echo "           Optional                         [-k] API key for 'https://network.pivotal.io'";
+        echo "                  Mandatory                      [-f] Path to 'diagnostic_report.json'";
+        echo "                  Optional                       [-k] API key for 'http://network.pivotal.op'";
         exit;
-        ;;
-     \?)
+      ;;
+      \?)
         echo "Error in command line parsing.";
         exit;
-        ;;
+      ;;
       esac
+  done
+  
+  for product_name in $(jq '.added_products.deployed[] | .name' $file_name | sed 's/"//g')
+  do
+    product_version=$(jq ".added_products.deployed[] | select(.name==\"${product_name}\") | .version" $file_name | awk -F "-" '{ print $1 }' | sed 's/"//g')
+    product_pivnet_slug="$product_name"
+    counter=0
+    
+    while [ $counter -le 1 ]
+    do
+      obj_jq=$(curl -X GET "$product_pivnet_url/$product_pivnet_slug" --silent)
+      
+      if [[ $(echo "$obj_jq" | jq ".status") != 404 ]]
+      then
+        fx_tmp_name $product_name $product_version "$obj_jq"
+        break;
+      else
+        if [[ $counter -eq 0 ]]
+        then
+          product_pivnet_slug=$(checkPivnetName $product_name)
+        else
+          echo "Unable to solve the Product Pivnet Slung Name for Product: $product_name"
+        fi
+        counter=$((counter+1))
+      fi
+    done
   done
 }
 
-function verify_api_key() {
-  auth_code=$(curl -X GET https://network.tanzu.vmware.com/api/v2/authentication -H "Authorization: $api_key" | jq '.status')
+function fx_tmp_name () {
+  # variables
+  local product_name=$1
+  local product_version=$2
+  local obj_jq=$3
+  local today_is=$(date +"%Y-%m-%d")
+  local product_id=""
+  local product_eogs_date=""
   
-  if [ $auth_code == 401 ]
+  # body
+  product_eogs_date=$(echo "$obj_jq" | jq ".releases[] | select(.version==\"${product_version}\") | .end_of_support_date" | sed 's/"//g')
+  product_id=$(echo "$obj_jq" | jq ".releases[] | select(.version==\"${product_version}\") | .id" | sed 's/"//g')
+  
+  if $(checkDate $product_eogs_date)
   then
-    echo "Unable To Verify API Key. Exiting Now..."
-    exit;
-  elif [ $auth_code == 200 ]
-  then
-    echo "Continuing.."
-  else
-    echo "Unknown Error. Exiting."
-    exit;
-  fi
-}
-
-function verify_json_file() {
-  if [ -z "$file_name_json" ]
-  then
-    echo "You need a file name.";
-  elif [ -f $file_name_json ]
-  then
-    if jq empty $file_name_json 2> /dev/null
+    if [[ $product_eogs_date < $today_is ]]
     then
-      return 1;
+      generateJSON 'diagnostic_report_all.json' $product_id $product_pivnet_slug $product_name $product_version $product_eogs_date
+      generateJSON 'diagnostic_report_eogs.json' $product_id $product_pivnet_slug $product_name $product_version $product_eogs_date
+    elif [[ $product_eogs_date == $today_is ]]
+    then
+      generateJSON 'diagnostic_report_all.json' $product_id $product_pivnet_slug $product_name $product_version $product_eogs_date
+      generateJSON 'diagnostic_report_eogs.json' $product_id $product_pivnet_slug $product_name $product_version $product_eogs_date
+      generateJSON 'diagnostic_report_supported.json' $product_id $product_pivnet_slug $product_name $product_version $product_eogs_date
     else
-      echo "File $file_name_json Is NOT Valid.";
-      exit;
+      generateJSON 'diagnostic_report_all.json' $product_id $product_pivnet_slug $product_name $product_version $product_eogs_date
+      generateJSON 'diagnostic_report_supported.json' $product_id $product_pivnet_slug $product_name $product_version $product_eogs_date
     fi
   else
-    echo "File $file_name_json No Exist. Please verify path.";
+    echo "Error with product eogs date which is $product_eogs_date, with a product name of $product_name using version $product_version"
+    echo "    $product_pivnet_url/$product_name"
   fi
 }
 
-function known_product_slugs_with_different_url() {
-  declare -A errSlug=(
+function generateJSON () {
+  # variables
+  local file=$1
+  local product_id=$2
+  local product_pivnet_slug=$3
+  local product_name=$4
+  local product_version=$5
+  local product_eogs_date=$6
+  local tanzu_network_url="https://network.pivotal.io/products/$product_name/releases/$product_id"
+  
+  # body
+  if [ ! -f $file ]
+  then
+    echo '{}' > $file
+  fi
+  
+  echo "$(jq ".deployed += [{ \"id\": \"$product_id\", \"pivnet_slug\": \"$product_pivnet_slug\", \"name\": \"$product_name\", \"version\": \"$product_version\", \"eogs_date\": \"$product_eogs_date\", \"url\": \"$tanzu_network_url\" }]" $file)" > $file
+}
+
+function checkDate () {
+  # variables
+  local idate=$1
+  
+  # body
+  if [[ $idate =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]
+  then
+    true
+  else
+    false
+  fi
+}
+
+function checkPivnetName () {
+  # variables
+  local product_name=$1
+  declare -A pivnet_slug_arr=(
     ["p-healthwatch2"]="p-healthwatch"
     ["p-healthwatch2-pas-exporter"]="p-healthwatch"
     ["metric-store"]="p-metric-store"
     ["appMetrics"]="apm"
+    ["p-antivirus"]="p-clamav-addon"
+    ["p-antivirus-mirror"]="p-clamav-addon"
   )
   
-  for index in ${!errSlug[@]}
+  for index in ${!pivnet_slug_arr[@]}
   do
     if [[ "$index" == "$product_name" ]]
     then
-      product_name="${errSlug[$index]}"
-      return 0
+      echo "${pivnet_slug_arr[$index]}"
+      return 1;
     fi
   done
+  
+  # If product name is not in the dictionary it outputs the f(x) input
+  echo "$product_name"
+}
+
+function verify_json_file () {
+  # variable
+  local file_name=$1
+  
+  # body
+  if [[ -z $file_name ]]
+  then
+    echo "You need a file name.";
+    exit;
+  elif [[ -f $file_name ]]
+  then
+    if jq empty $file_name 2> /dev/null
+    then
+      true;
+    else
+      echo "File $file_name is NOT valid JSON file.";
+      exit;
+    fi
+  else
+    echo "File $file_name Does Not Exist! Please verify file path.";
+    exit;
+  fi
 }
 
 
 
-#************************************************
+#***********************************
 # MAIN
-#************************************************
-get_options $@
-today_date=$(date +"%Y-%m-%d")
-
-rm -f main.csv
-rm -f diagnostic_report_all.*
-rm -f diagnostic_report_eogs.*
-rm -f diagnostic_report_supported.*
-
-
-# Parsing the diagnostic file from the support bundle(s)
-for product_name in $(jq '.added_products.deployed[] | .name' $file_name_json | sed 's/"//g')
-do
-  
-  # Get Product Version Of Current Product Name
-  product_version=$(jq ".added_products.deployed[] | select(.name==\"${product_name}\") | .version" $file_name_json | awk -F "-" '{ print $1 }' | sed 's/"//g')
-  
-  # Checking Slugs
-  known_product_slugs_with_different_url
-  
-  is_error=$(curl -X GET https://network.tanzu.vmware.com/api/v2/product_details/$product_name | jq ".status")
-
-  if [[ $is_error != 404 ]]
-  then
-    # Get End Of General Support Date
-    product_eogs=$(curl -X GET https://network.tanzu.vmware.com/api/v2/product_details/$product_name | jq ".releases[] | select(.version==\"${product_version}\") | .end_of_support_date" | sed 's/"//g')  
-  
-    # DISPLAY
-    # Get Todays Date
-    today_epoch_date=$(date -j -f "%Y-%m-%d" $today_date "+%s")
-    
-    product_eogs_epoch_date=$(date -j -f "%Y-%m-%d" $product_eogs "+%s")
-    
-    if [[ $product_eogs_epoch_date < $today_epoch_date ]];
-    then
-      echo "$product_name,$product_version,$product_eogs,$product_eogs_epoch_date" >> diagnostic_report_all.csv
-      echo "$product_name,$product_version,$product_eogs,$product_eogs_epoch_date" >> diagnostic_report_eogs.csv
-    else
-      echo "$product_name,$product_version,$product_eogs,$product_eogs_epoch_date" >> diagnostic_report_all.csv
-      echo "$product_name,$product_version,$product_eogs,$product_eogs_epoch_date" >> diagnostic_report_supported.csv
-    fi
-  else
-    # There's some error with the EOGS date
-    echo "$product_name,$product_version,$product_eogs" >> main.csv
-  fi
-done
+#***********************************
+__main__ $@
